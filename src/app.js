e845edd882c2190965b4b2cff690bd60f8a1a01d
sanka <sankamidhilesh@gmail.com>
2025-11-13 12:00:26 +0530

    updated

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express"); // import from node modules
+
+const app = express(); // calling the express
+app.use((req, res) => {
+  res.send("server is updating automatically");
+});
+
+app.listen(2000, () => {
+  console.log("server is successfully updated on port 4444");
+});
+   
