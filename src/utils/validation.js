d8b819979fadd9a52aaead07be575921aaf133f7
sanka <sankamidhilesh@gmail.com>
2025-11-18 18:45:27 +0530

    updated

---
+++ src/utils/validation.js
@@ -0,0 +1,16 @@
+const validator = require("validator");
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+
+  if (!firstName || !lastName) {
+    throw new Error("name is not valid!");
+  } else if (firstName.length < 4 || firstName.length > 50) {
+    throw new Error("first name should be 4 - 50 characters");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("email is not valid!");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("password is not strong");
+  }
+};
+
+module.exports={validateSignUpData}
